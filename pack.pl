4edd59841890f6dee6c7c26bbca20780c76ea429
honnix <hxliang1982@gmail.com>
2016-09-02 14:25:54 +0200

    initial project

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(yaml).
+version('0.1').
+title('YAML parser').
+keywords([yaml, parser]).
+author('Honnix Liang', 'hxliang1982@gmail.com').
+packager('Honnix Liang', 'hxliang1982@gmail.com').
+maintainer('Hongxin Liang', 'hxliang1982@gmail.com').
+home('https://github.com/honnix/yaml').
+download('https://github.com/honnix/yaml/archive/v0.1.zip').
